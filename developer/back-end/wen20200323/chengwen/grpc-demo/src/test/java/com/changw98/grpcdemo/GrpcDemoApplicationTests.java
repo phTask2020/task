bee7e5d90653fb6f1cec05889021ec35de3641d0
Administrator <changw98@126.com>
2020-04-01 20:30:41 +0800

    the homework

---
+++ developer/back-end/wen20200323/chengwen/grpc-demo/src/test/java/com/changw98/grpcdemo/GrpcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.changw98.grpcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
