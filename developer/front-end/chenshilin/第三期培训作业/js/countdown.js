a65bd2656d1e388e9a5ab5c6aa6896a064370604
chenshil <3395408372@qq.com>
2020-04-12 15:45:38 +0800

    修改倒计时显示问题

--- developer/front-end/chenshilin/第三期培训作业/js/countdown.js
+++ developer/front-end/chenshilin/第三期培训作业/js/countdown.js
@@ -1,8 +1,9 @@
-function showDate(){
+
+function showDate (){
     // 设置活动结束时间
-    var dateStr = "2020-04-11 22:35:00";
+    var dateStr = "2020-04-12 15:32:00";
     // 转换为时间
-    var endDate = new Date(timeStr)
+    var endDate = new Date(dateStr)
     // 获取系统当前时间
     var nowDate = new Date();
     // 获取活动剩余时间的毫秒值
@@ -10,25 +11,40 @@ function showDate(){
     // 转换成天数
     var days = parseInt(ms/1000/60/60/24,10);
     // 转换成小时
-    var hours = parseInt(ms/1000/60/60%24,10);
+    var hours = parseInt(ms/1000/60/60 % 24,10);
     // 转换成分钟
-    var minutes = parseInt(ms/1000/60%60,10);
+    var minutes = parseInt(ms/1000/60 % 60,10);
     // 转换成秒钟
-    var seconds = parseInt(ms/1000%60,10);
-
-    // 在0-9的数前加上0
-    // days = checkTime(days);
-    // hours = checkTime(hours);
-    // minutes = checkTime(minutes);
-    // seconds = checkTime(seconds);
+    var seconds = parseInt(ms/1000 % 60,10);
 
     //在页面上显示
-    document.getElementById(hour).innerHTML = hours; 
-    document.getElementById(minute).innerHTML = minutes;
-    document.getElementById(second).innerHTML = seconds;
+    if (hours>=10) {
+        document.getElementById("hour").innerHTML = hours; 
+    } else if(hours<10 && hours>=0){
+        document.getElementById("hour").innerHTML = "0" + hours;
+    }else{
+        // 活动结束后显示00
+        document.getElementById("hour").innerHTML = "00";
+    };
+    if (minutes>=10) {
+        document.getElementById("minute").innerHTML = minutes;
+    } else if(minutes<10 && minutes>=0){
+        document.getElementById("minute").innerHTML = "0" + minutes;
+    }else{
+        document.getElementById("minute").innerHTML = "00";
+    };
+    if (seconds>=10) {
+        document.getElementById("second").innerHTML = seconds;
+    } else if (seconds<10 && seconds>=0) {
+        document.getElementById("second").innerHTML = "0" + seconds;
+    }else{
+        document.getElementById("second").innerHTML = "00";
+    }  
+    // 递归调用函数
+    window.setTimeout("showDate()",1000);
 }
-// 每隔一秒调用一次函数
+// 页面加载完调用
 window.onload = function(){
-    window.setTimeout("showDate",1000);
+    showDate();  
 }
     
